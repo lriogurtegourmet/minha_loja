017789ed63c3de14d4acdba3ef102a4715de2838
eclipseCJP <cris.tecwodd@gmail.com>
2024-02-15 15:00:22 -0400

    ajustando o menu responsivo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function menu(){
+    //Esta linha está selecionando um elemento do DOM com o ID "menu-mobile" e armazenando-o na variável menu.
+    let menu = document.getElementById("menu-mobile")
+
+    if(menu.style.display == "none")
+        menu.style.display = "block"
+
+    else 
+        menu.style.display = "none"
+}
+
+alert("EM CONSTRUÇÃO! :D")
